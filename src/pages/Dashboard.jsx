6358ec17b47c1e7220d8266e374a84603ec0de97
Mehmet F. K <mk@atina.at>
2023-04-19 17:09:58 +0200

    Users link changed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -117,7 +117,7 @@ export default function Dashboard() {
     {
       text: "Benutzer",
       icon: <PeopleAltIcon />,
-      nav: "users",
+      nav: "",
     },
     {
       text: "Mobile Buchungen",
